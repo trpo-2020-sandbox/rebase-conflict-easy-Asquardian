58b035d9279566e7658b872670154f0989a75a96
srwxrwxrwx <srwxrwxrwx@users.noreply.github.com>
2017-03-04 17:57:36 +0700

    Add more elements to array

--- main.c
+++ main.c
@@ -18,16 +18,12 @@ int min_element(int* arr, int size)
 
 int main()
 {
-    int arr[] = {3, 1, 4, 1, 5, 9, 2};
-
-    const int min = min_element(arr, N_ELEMENTS(arr));
-
+    int arr[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
+    const int min = min_element(arr, 10);
     printf("Array: ");
-    int i = 0;
-    printf("%d", arr[i]);
-    ++i;
-    for (; i < N_ELEMENTS(arr); ++i) {
-        printf(", %d", arr[i]);
+    int i;
+    for (i = 0; i < 10; ++i) {
+        printf("%d ", arr[i]);
     }
     printf("\n");
 
